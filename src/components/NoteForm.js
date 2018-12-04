611602f6d3baa1abc4be27e1583cab81b36b90c3
caoviet <vietanhcao1994@gmail.com>
2018-12-04 13:22:22 +0700

    135 hien thi noi dung day vao

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 const mapStateToProps = (state, ownProps) => {
     return {
-        test: state.test
+        item: state.editItem
     }
 }
 const mapDispatchToProps = (dispatch, ownProps) => {
@@ -31,19 +31,19 @@ export default connect(mapStateToProps, mapDispatchToProps)(class NoteForm exten
         let obj = { ...this.state };
         this.props.addDataStore(obj);
     }
-    render() {
+    render() {     
         return (
             <div className="col-sm-4">
                 <h3>Sửa nội dung note</h3>
                 <form action="">
                     <div className="form-group">
                         <label htmlFor="noteTitle">Tiêu đề note</label>
-                        <input type="text" onChange={this.isChange.bind(this)} className="form-control" name="noteTitle" id="noteTitle" aria-describedby="helpIdNotetitle" placeholder="Tiêu đề note" />
+                        <input type="text" onChange={this.isChange.bind(this)} className="form-control" name="noteTitle" id="noteTitle" aria-describedby="helpIdNotetitle" placeholder="Tiêu đề note" defaultValue={this.props.item.noteTitle} />
                         <small id="helpIdNotetitle" className="form-text text-muted">Điền tiêu đề vào đây</small>
                     </div>
                     <div className="form-group">
                         <label htmlFor="noteTitleContent">Content note</label>
-                        <textarea type="text" onChange={this.isChange.bind(this)} className="form-control" name="noteTitleContent" id="noteTitleContent" aria-describedby="helpIdNotetitleContent" placeholder="Content note" defaultValue={" "} />
+                        <textarea type="text" onChange={this.isChange.bind(this)} className="form-control" name="noteTitleContent" id="noteTitleContent" aria-describedby="helpIdNotetitleContent" placeholder="Content note" defaultValue={this.props.item.noteTitleContent} />
                         <small id="helpIdNotetitleContent" className="form-text text-muted">Điền Content vào đây</small>
                     </div>
                     <button type="reset" onClick={this.addData.bind(this)} className="btn btn-primary btn-block">Save</button>
