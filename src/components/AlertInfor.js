e24a82f73e39eff3fd487ce43df6938b20b89a30
caoviet <vietanhcao1994@gmail.com>
2018-12-05 10:03:27 +0700

    146 set time show hide

--- src/components/AlertInfor.js
+++ src/components/AlertInfor.js
@@ -10,18 +10,23 @@ const mapStateToProps = (state, ownProps) => {
 }
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-
+    changeAlterOff: () => {
+      dispatch({ type: "Change_Alter_Off" })
+    }
   }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(class AlertInfor extends Component {
+  handleDismiss(){
+    this.props.changeAlterOff();
+  }
   render() {
     if(!this.props.alertShow){
       return false;
     }
     return (
       <AlertContainer>
-        <Alert type="info">Hello, world</Alert>
+        <Alert type="info" onDismiss={this.handleDismiss.bind(this)} timeout={2000} >Hello, world</Alert>
       </AlertContainer>
     )
   }
